bcf8068610ba8a0ec18892f65143d39b9241209b
Emmanuel Jammes <emmanuel.jammes@ixxi.biz>
2017-11-22 13:56:09 +0100

    ajout requirements-dev modif test_jenkins

--- src/tests/test_jenkins.py
+++ src/tests/test_jenkins.py
@@ -1,13 +1,14 @@
 import pytest
 import os
 
-from ..buildbox.ci import BuildBoxJenkins
+from buildbox.ci import BuildBoxJenkins
 
 # TODO: find an infrastructure agnostic way to test api or make the test optional
 def test_jenkins():
     jenkins_url = "https://factory-ixxi.noisy.ratp/jenkins"
     view = "API_IXXI_1545"
-    f = open(os.path.expanduser('~') + "/.mdp", "r")
+    filename = os.path.join(os.path.expanduser('~'), ".mdp")
+    f = open(filename, "r")
     username,password=f.read().rstrip().split(":")
     jenkins = BuildBoxJenkins(jenkins_url, username, password)
     info = jenkins.get_jobs_from_view(view)
